3125f543c8c4ebb23593742603d81df18bf9391d
wgeesey <89809942+wgeesey@users.noreply.github.com>
2024-12-02 08:26:30 -0500

    Update automation.py

--- automation.py
+++ automation.py
@@ -62,30 +62,7 @@ assert 'College Football Scores - 2024 Season - ESPN' in chrome_browser.title
 # Wait for user input before closing the browser (this gives the user a chance to check the result)
 input("press enter to close browser")
 
-# The code below is commented out but shows an alternative example of using Selenium to interact with a form.
-# You can refer to this for future exploration of Selenium's capabilities.
-# From my Udemy Course.
 
-# chrome_browser = webdriver.Chrome()
-# chrome_browser.maximize_window()
-# chrome_browser.get('https://demo.seleniumeasy.com/basic-first-form-demo.html')
- 
-# # This solves the issue with the Popup for those that encounter it:
-# chrome_browser.implicitly_wait(2)
-# popup =chrome_browser.find_element(By.ID, 'at-cv-lightbox-close')
-# popup.click()
- 
-# # Example of interacting with a form on the page
-# user_message = chrome_browser.find_element(By.ID, 'user-message')
-# user_message.clear()
-# user_message.send_keys('I AM EXTRA COOOOL')
- 
-# time.sleep(2)
-# show_message_button = chrome_browser.find_element(By.CLASS_NAME, 'btn-default')
-# show_message_button.click()
- 
-# output_message = chrome_browser.find_element(By.ID, 'display')
-# assert 'I AM EXTRA COOOOL' in output_message.text
 
 
 
